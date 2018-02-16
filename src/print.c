71c3474cf2bd704378455b533b39ad622b250ac2
Gharieni MELEK <gmelek@e1r12p18.42.fr>
2018-02-16 16:44:05 +0100

    a suivre

--- src/print.c
+++ src/print.c
@@ -6,7 +6,7 @@
 /*   By: gmelek <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/12/01 21:47:24 by gmelek            #+#    #+#             */
-/*   Updated: 2018/01/23 12:23:41 by gmelek           ###   ########.fr       */
+/*   Updated: 2018/02/16 16:29:48 by gmelek           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include "ft_ls.h"
@@ -21,6 +21,9 @@ void	print(d_list *d_dir, int m,int n,t_flags flags)
 	char				*dat;
 	char *c;
 	int i;
+	int major;
+	int minor;
+		
 	if(flags.flag_l == 1)
 	{
 		//mtime = NULL;
@@ -29,7 +32,9 @@ void	print(d_list *d_dir, int m,int n,t_flags flags)
 		time_tmp = malloc(sizeof(time_t));
 		int i;
 		// DROIT >>
-		ft_putchar( (S_ISLNK(d_dir->content->st_mode)) ? 'l' : 
+		ft_putchar( (S_ISCHR(d_dir->content->st_mode)) ? 'c' :
+				(S_ISBLK(d_dir->content->st_mode)) ? 'b' :
+				(S_ISLNK(d_dir->content->st_mode)) ? 'l' : 
 				(S_ISDIR(d_dir->content->st_mode)) ? 'd' : '-');
 		ft_putchar( (d_dir->content->st_mode & S_IRUSR) ? 'r' : '-');
 		ft_putchar( (d_dir->content->st_mode & S_IWUSR) ? 'w' : '-');
@@ -41,7 +46,7 @@ void	print(d_list *d_dir, int m,int n,t_flags flags)
 		(d_dir->content->st_mode & S_IXGRP) ? 'x' : '-');
 		ft_putchar( (d_dir->content->st_mode & S_IROTH) ? 'r' : '-');
 		ft_putchar( (d_dir->content->st_mode & S_IWOTH) ? 'w' : '-');
-		ft_putstr( (d_dir->content->st_mode & S_ISVTX) ? "T" :
+		ft_putstr( (d_dir->content->st_mode & S_ISVTX) ? "t  " :
 		(d_dir->content->st_mode & S_IXOTH) ? "x  " : "-  ");
 		// NOMBRE DE LIEN >>
 		c = ft_itoa(d_dir->content->st_nlink);
@@ -53,17 +58,32 @@ void	print(d_list *d_dir, int m,int n,t_flags flags)
 		// USER  >>
 		if ((pwd = getpwuid(d_dir->content->st_uid)) != NULL)
 			ft_putstr(pwd->pw_name);
+		int k = ft_strlen(pwd->pw_name) + m + 2;
+		//while (k--)
+		//	ft_putchar(' ');
 		ft_putstr("  ");
 		// GROUPE >>
 		if ((grp = getgrgid(d_dir->content->st_gid)) != NULL)
 			ft_putstr(grp->gr_name);
 		ft_putstr("  ");
+		if(S_ISCHR(d_dir->content->st_mode) || S_ISBLK(d_dir->content->st_mode))
+		{
+			major = (int)major(d_dir->content->st_rdev);
+			minor = (int)minor(d_dir->content->st_rdev);
+			ft_putnbr(major);
+			ft_putstr(",   ");
+			ft_putnbr(minor);
+			
+		}
+		else
+		{
 		// SIZE >>
 		 c = ft_itoa(d_dir->content->st_size);
 		while (m-- - ft_strlen(c))
 			ft_putchar(' ');
 		free(c);
 		ft_putnbr(d_dir->content->st_size);
+		}
 		ft_putchar(' ');
 		//TIME && DATE
 		ttime = time(NULL);
